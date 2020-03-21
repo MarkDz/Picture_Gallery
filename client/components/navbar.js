e6ebcad5a1e24651f04a5a00fd13a587e3f006f5
Mark Dziwirek <marekdz@gmail.com>
2020-03-21 11:35:36 -0400

    change to navbar add styling

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -4,43 +4,146 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
 import FileUpload from './fileUpload'
-//import styled from 'styled-components'
+import styled from 'styled-components'
 
-// const Button = styled.div`
-//   background: 'white';
-//   font-size: 1em;
-//   margin: 0.5em;
-//   margin-top: 30px;
-//   padding: 0.5em 0.5em;
-//   font-family: Consolas, monaco, monospace;
-// `
-// const LinkButtonsGroup = styled.div`
-//   display: flex;
-//   flex: wrap;
-//   align-items: left;
-//   justify-content: left;
-//   background-color: white;
-// `
-// const Title = styled.div`
-//   color: rebeccapurple;
-//   margin: 10px;
-//   font-family: Consolas, monaco, monospace;
-// `
+const NavItem = styled(Link)`
+  text-decoration: none;
+  color: #111;
+  display: inline-block;
+  white-space: nowrap;
+  margin: 0 1vw;
+  transition: all 200ms ease-in;
+  position: relative;
+
+  :after {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    width: 0%;
+    content: '.';
+    color: transparent;
+    background: goldenrod;
+    height: 1px;
+    transition: all 0.4s ease-in;
+  }
+
+  :hover {
+    color: goldenrod;
+    ::after {
+      width: 100%;
+    }
+  }
+
+  @media (max-width: 768px) {
+    padding: 20px 0;
+    font-size: 1.5rem;
+    z-index: 6;
+  }
+`
+
+const Navigation = styled.nav`
+  height: 10vh;
+  display: flex;
+  background-color: #fff;
+  position: relative;
+  justify-content: space-between;
+  text-transform: uppercase;
+  border-bottom: 2px solid #33333320;
+  margin: 0 auto;
+  padding: 0 5vw;
+  z-index: 2;
+  align-self: center;
+
+  @media (max-width: 768px) {
+    position: sticky;
+    height: 8vh;
+    top: 0;
+    left: 0;
+    right: 0;
+    left: 0;
+  }
+`
+
+const Toggle = styled.div`
+  display: none;
+  height: 100%;
+  cursor: pointer;
+  padding: 0 10vw;
+
+  @media (max-width: 768px) {
+    display: flex;
+  }
+`
+
+const Navbox = styled.div`
+  display: flex;
+  height: 100%;
+  justify-content: flex-end;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    position: fixed;
+    width: 100%;
+    justify-content: flex-start;
+    padding-top: 10vh;
+    background-color: #fff;
+    transition: all 0.3s ease-in;
+    top: 8vh;
+    left: ${props => (props.open ? '-100%' : '0')};
+  }
+`
+
+const Hamburger = styled.div`
+  background-color: #111;
+  width: 30px;
+  height: 3px;
+  transition: all 0.3s linear;
+  align-self: center;
+  position: relative;
+  transform: ${props => (props.open ? 'rotate(-45deg)' : 'inherit')};
+
+  ::before,
+  ::after {
+    width: 30px;
+    height: 3px;
+    background-color: #111;
+    content: '';
+    position: absolute;
+    transition: all 0.3s linear;
+  }
+
+  ::before {
+    transform: ${props =>
+      props.open ? 'rotate(-90deg) translate(-10px, 0px)' : 'rotate(0deg)'};
+    top: -10px;
+  }
+
+  ::after {
+    opacity: ${props => (props.open ? '0' : '1')};
+    transform: ${props => (props.open ? 'rotate(90deg) ' : 'rotate(0deg)')};
+    top: 10px;
+  }
+`
 
 const Navbar = ({handleClick, isLoggedIn}) => {
   return (
     <div>
-      <div>
+      <Navigation>
+        <FileUpload />
         {isLoggedIn ? (
           <div>
             {/* The navbar will show these links after you log in */}
-            <h1>My Dashboard</h1>
-            <FileUpload />
-            <Link to="/home">Home</Link>
-            <Link to="/paginated">My Pictures</Link>
-            <a href="#" onClick={handleClick}>
-              Logout
-            </a>
+            {/* <h1>My Dashboard</h1> */}
+
+            <NavItem to="/home">Home</NavItem>
+            <NavItem to="/paginated">My Pictures</NavItem>
+            <NavItem>
+              <a href="#" onClick={handleClick}>
+                Logout
+              </a>
+            </NavItem>
           </div>
         ) : (
           <div>
@@ -50,7 +153,7 @@ const Navbar = ({handleClick, isLoggedIn}) => {
             <Link to="/signup">Sign Up</Link> */}
           </div>
         )}
-      </div>
+      </Navigation>
     </div>
   )
 }
